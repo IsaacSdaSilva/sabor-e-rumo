004a7bf9084a3d43e6838c830ddf553d3efc0863
IsaacSdaSilva <is.aaac.is20@gmail.com>
2026-03-31 19:45:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function pedir(produto, idQuantidade) {
      alert("Por favor,Coloca teu nome 😄.");
      return;
  }
- let mensagem = `Olá, Me chamo ${nome} e gostaria de pedir ${quantidade} x ${produto} 🍮`;
+ let mensagem = `Olá, Me chamo ${nome} e gostaria de pedir ${quantidade} x ${produto} 🍮 
+ Sei que o prazo de entrega é de até 2 dias.`;
  window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`, '_blank');
 }
